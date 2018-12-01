065c19807aa754528bad11f00d129617ea3ab6a4
oba2311 <oba2311@gmail.com>
2018-12-01 21:24:56 +0200

    working chart for jews

--- BarChart/BarChart.js
+++ BarChart/BarChart.js
@@ -31,6 +31,70 @@ var dataset = [
     550.100,
 ]
 
+////////////////////////////////////////////////////////////////////////
+/////////xaxis
+var XlabelsData = [
+    {xaxis: '1-jan-1525'},
+    {xaxis: '1-jan-1538'},
+    {xaxis: '1-jan-1553'},
+    {xaxis: '1-jan-1838'},
+    {xaxis: '1-jan-1846'},
+    {xaxis: '1-jan-1851'},
+    {xaxis: '1-jan-1862'},
+    {xaxis: '1-jan-1876'},
+    {xaxis: '1-jan-1882'},
+    {xaxis: '1-jan-1885'},
+    {xaxis: '1-jan-1905'},
+    {xaxis: '1-jan-1922'},
+    {xaxis: '1-jan-1931'},
+    {xaxis: '1-jan-1944'},
+    {xaxis: '1-jan-1948'},
+    {xaxis: '1-jan-1953'},
+    {xaxis: '1-jan-1967'},
+    {xaxis: '1-jan-1980'},
+    {xaxis: '1-jan-1985'},
+    {xaxis: '1-jan-1987'},
+    {xaxis: '1-jan-1990'},
+    {xaxis: '1-jan-1995'},
+    {xaxis: '1-jan-2000'},
+    {xaxis: '1-jan-2004'},
+    {xaxis: '1-jan-2007'},
+    {xaxis: '1-jan-2011'},
+    {xaxis: '1-jan-2014'},
+    {xaxis: '1-jan-2016'}
+]
+
+// var xAxis = "xaxis";
+
+
+// var parseTime = d3.timeParse("%d-%b-%y");
+// XlabelsData.forEach(function(d) {
+//     d.xaxis = parseTime(d.xaxis);
+// });
+// x0 = d3.scaleTime().range([0, svgWidth-100]);
+// x0.domain(d3.extent(XlabelsData, function(d) {
+//     return d[xAxis]
+// }));
+
+
+
+
+// g.append("g")
+// .attr("class", "axis")
+// .attr("transform", "translate(0," + ypoint + ")")
+// .call(d3.axisBottom(x0).tickFormat(d3.timeFormat("%Y-%m-%d")))
+// .append("text")
+// .attr("x", width / 2)
+// .attr("y", margin.bottom * 0.9)
+// .attr("dx", "0.32em")
+// .attr("fill", "#000")
+// .attr("font-weight", "bold")
+// .attr("text-anchor", "start")
+// .text("YYYY-MM-DD Format");
+
+///////////////////////////////////////////////////////////////
+
+
 var svgWidth = 1500, svgHeight = 570, barPadding = 5;
 var barWidth = (svgWidth / dataset.length);
 
@@ -68,6 +132,20 @@ var text = svg.selectAll('text')
         return barWidth *i;
     })
     .attr('fill', "white");
+
+var labels = svg.selectAll('labels')
+    .data(dataset)
+    .enter()
+    .append('labels')
+    .text(xAxis)
+    .attr('y', function(d,i){
+        return svgHeight  - 200;
+    })
+    .attr('x', function(d,i){
+        return barWidth *i;
+    })
+    .attr('fill', "red");
+
 
     svg.append("text")      // text label for the x axis
         .attr("x", svgHeight+200)
---
+++ TableGeneralScraper.ipynb
@@ -0,0 +1,17 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+
+url='https://www.btselem.org/hebrew/jerusalem/building_starts_statistics'
+#Create a handle, page, to handle the contents of the website
+page = requests.get(url)
+#Store the contents of the website under doc
+doc = lh.fromstring(page.content)
+#Parse data that are stored between <tr>..</tr> of HTML
+tr_elements = doc.xpath('//tr')
+
+print(tr_elements)
+
+
+# code inspired by:
+# https://towardsdatascience.com/web-scraping-html-tables-with-python-c9baba21059
